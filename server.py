30030b95dbe975b9d9166c5f215de07d39117988
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2020-02-07 01:15:22 +0530

    created basic flask server, added template files

--- server.py
+++ server.py
@@ -1,22 +1,11 @@
-import io
-import random
-from flask import Response
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+from flask import Flask, render_template
 
-@app.route('/plot.png')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
+app = Flask(__name__)
 
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
-    return fig
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
