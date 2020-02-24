386b5b07607fa7aa8f906039852867ae192c5365
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2020-02-24 23:55:28 +0530

    implemented real time quantization of audio signal

--- audio_test.py
+++ audio_test.py
@@ -1,19 +1,40 @@
 import pyaudio
-import numpy as np
+import numpy
+import struct
+import matplotlib.pyplot as plt
 
-CHUNK = 2**11
-RATE = 44100
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 8000
+CHUNK = 1024
+RECORD_SECONDS = 3
 
-p=pyaudio.PyAudio()
-stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
-              frames_per_buffer=CHUNK)
+audio = pyaudio.PyAudio()
 
-for i in range(int(10*44100/1024)): #go for a few seconds
-    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-    peak=np.average(np.abs(data))*2
-    bars="#"*int(50*peak/2**16)
-    print("%04d %05d %s"%(i,peak,bars))
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
 
+print("recording...")
+
+frames = []
+result_arr = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(numpy.frombuffer(data))
+    data_int = numpy.array(struct.unpack(str(2*CHUNK) + 'B', data))[::2]
+    result_arr.append(data_int)
+
+# numpydata = numpy.hstack(frames)
+
+#plotting last tuple
+plt.plot(numpy.arange(0, len(data_int)), data_int)
+plt.show()
+
+print("finished recording")
+# stop Recording
 stream.stop_stream()
 stream.close()
-p.terminate(
+audio.terminate()
