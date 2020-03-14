c8ecb1e8065e4866d49d3b870abc79a25fae98cb
Pratyush Kerhalkar <pratyushkerhalkar@gmail.com>
2020-03-15 01:25:21 +0530

    added routes for all the line codes

--- server.py
+++ server.py
@@ -1,55 +1,54 @@
 from flask import Flask, render_template, Response, url_for
 from datetime import datetime
-import random
 import json
 import time
 import audioEngine
-import matplotlib.pyplot as plt
 import lineCodes
+import numpy as np
 
 app = Flask(__name__)
-random.seed()
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route('/test')
-# def randomChart():
-#     def makeRandom():
-#         while(True):
-#             binData = [random.randint(0, 1) for i in range(255)]
-#             for item in binData:
-#                 json_data = json.dumps(
-#                     {'key': item, 'value': item})
-#                 yield f"data:{json_data}\n\n"
-#                 time.sleep(0.08)
-#     return Response(makeRandom(),  mimetype='text/event-stream')
+def generateData(function):
+    for item in audioEngine.generateAudioChunks(RATE=8000):
+        x, y = function(np.array(item))
+        for i in y:
+            json_data = json.dumps(
+                {'key': int(i), 'value': int(i)})
+            yield f"data:{json_data}\n\n"
+            time.sleep(0.08)
 
 
-@app.route('/test')
-def randomChart():
-    def makeRandom():
-        for item in audioEngine.generateAudioChunks(RATE=8000):
-            for i in item:
-                json_data = json.dumps(
-                    {'key': int(i), 'value': int(i)})
-                yield f"data:{json_data}\n\n"
-                time.sleep(0.08)
-    return Response(makeRandom(),  mimetype='text/event-stream')
+@app.route('/unipolar')
+def unipolarData():
+    return Response(generateData(lineCodes.unipolar),  mimetype='text/event-stream')
 
+@app.route('/bipolar')
+def bipolarData():
+    return Response(generateData(lineCodes.bipolar),  mimetype='text/event-stream')
 
-# @app.route('/test')
-# def randomChart():
-#     def makeRandom():
-#         for item in audioEngine.generateAudioChunks(RATE=8000):
-#             for i in item:
-#                 json_data = json.dumps(
-#                     {'key': int(i), 'value': int(i)})
-#                 yield f"data:{json_data}\n\n"
-#                 time.sleep(0.08)
-#     return Response(makeRandom(),  mimetype='text/event-stream')
+@app.route('/RZ')
+def RZData():
+    return Response(generateData(lineCodes.RZ),  mimetype='text/event-stream')
 
+@app.route('/NRZ-I')
+def NRZ_IData():
+    return Response(generateData(lineCodes.NRZ_I),  mimetype='text/event-stream')
+
+@app.route('/NRZ-L')
+def NRZ_LData():
+    return Response(generateData(lineCodes.NRZ_L),  mimetype='text/event-stream')
+
+@app.route('/manchester')
+def manchesterData():
+    return Response(generateData(lineCodes.manchester),  mimetype='text/event-stream')
+
+@app.route('/differential-manchester')
+def differentialManchesterData():
+    return Response(generateData(lineCodes.differential_manchester),  mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     const context = document.getElementById('canvas').getContext('2d');
     const chart = new Chart(context, config);
-    const source = new EventSource("/test");
+    const source = new EventSource("/manchester");
 
     source.onmessage = function (event) {
         const data = JSON.parse(event.data);
